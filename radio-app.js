1bb11fe35f7dfe016be6075b7f82a6dc02ce35e9
Francog0711 <francogabrielalconchel@gmail.com>
2024-02-19 01:48:03 -0300

    modificacion varias

--- radio-app.js
+++ radio-app.js
@@ -1,19 +1,22 @@
 const radioUrl = "URL_DE_TU_RADIO";
-
   const audio = new Audio(radioUrl);
   const playBtn = document.getElementById("playBtn");
   const pauseBtn = document.getElementById("pauseBtn");
   const stopBtn = document.getElementById("stopBtn");
+  const radioStatus = document.getElementById("radioStatus");
 
   playBtn.addEventListener("click", function() {
       audio.play();
+      radioStatus.textContent = "En línea";
   });
 
   pauseBtn.addEventListener("click", function() {
       audio.pause();
+      radioStatus.textContent = "Parado";
   });
 
   stopBtn.addEventListener("click", function() {
       audio.pause();
       audio.currentTime = 0;
+      radioStatus.textContent = "Fuera de servicio";
   });
